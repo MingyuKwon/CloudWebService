ede9bc43a54b57b5c1ea080da11f487077e3983a
Kwon Mingyu <kmgmg230601@gmail.com>
2023-11-24 19:17:42 +0900

    지도 띄우기 성공

--- app/src/main/java/com/example/cloudwebservice5/MainActivity.kt
+++ app/src/main/java/com/example/cloudwebservice5/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.cloudwebservice5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.example.cloudwebservice5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            MapButton.setOnClickListener {
+                moveToOtherActivity("MapActivity")
+            }
+        }
+
+    }
+
+    fun moveToOtherActivity(activityName: String) {
+        try {
+            var realactivityName  = "com.example.cloudwebservice5." + activityName
+            val activityClass = Class.forName(realactivityName).kotlin.java
+            val intent = Intent(this, activityClass)
+            startActivity(intent)
+        } catch (e: ClassNotFoundException) {
+            // 클래스가 찾을 수 없는 경우의 처리
+            e.printStackTrace()
+        } catch (e: Exception) {
+            // 기타 예외 처리
+            e.printStackTrace()
+        }
     }
 }
